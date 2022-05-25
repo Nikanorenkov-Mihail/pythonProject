10f8970425a91c11c03b8f3c0240aaeaa4751c65
Nikanorenkov-Mihail <nikanorenkov.md@edu.spbstu.ru>
2022-05-25 17:01:33 +0300

    Final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from sys import stderr
 from panflute import *
+from sys import stderr
 
-headers = []
+List = []
 
 def prepare(input_doc):
     input_doc.replace_keyword("BOLD", Strong(Str("BOLD")))
@@ -11,20 +11,20 @@ def upper_headers(now_element, doc):
         return Header(Str(stringify(now_element).upper), level=now_element.level)
 
 
-def header_attention(now_element, doc):
+def header_error(now_element, doc):
     if isinstance(now_element, Header):
         text = stringify(now_element)
-        if text in headers:
+        if text in List:
             print("Repeat headlines: " + text, file=stderr)
         else:
-            headers.append(text)
+            List.append(text)
 
 
 def finalize(doc):
     pass
 
 def main(doc=None):
-    return run_filters([upper_headers, header_attention], prepare=prepare, finalize=finalize, doc=doc)
+    return run_filters([upper_headers, header_error], prepare=prepare, finalize=finalize, doc=doc)
 
 if __name__ == '__main__':
     main()
